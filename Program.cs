e1f41b1a9a81b49bae77a7a64b09291ee980bd72
Kalas12 <vie95@mail.ru>
2016-02-07 14:46:46 +0600

    Создать рабочую сортировку

--- Program.cs
+++ Program.cs
@@ -21,43 +21,53 @@ namespace LogSortByAutor
             string line;
             using (StreamReader file = new StreamReader(fileFullPath))
                 {
-                for (int i = 0; i <= fileFullPath.Length; i++)
+                while (file.EndOfStream != true)
                     {
                     line = file.ReadLine();
-                    if (line.StartsWith("commit"))
-                        {
-                         Commit commit = new Commit();
-                        Commits.Add(commit);
-                        }
 
-                        if (line.StartsWith("Author:"))
+                    if (line.StartsWith("commit") == true)
+                        {
+                        Commit commit = new Commit();
+                        Commits.Add(commit);
+                        line = file.ReadLine();
+                        if (line.StartsWith("commit") != true)
                             {
-                            Commits[i-1].Autor = line;
+                            if (line.StartsWith("Author:"))
+                                {
+                                commit.Autor = line;
+                                line = file.ReadLine();
+                                }
+
+                            if (line == null)
+                                {
+                                line = file.ReadLine();
+                                }
+                            while (line.StartsWith("Author:") != true && line.StartsWith("commit") != true)
+                                {
+                                commit.Massage = commit.Massage + line;
+                                line = file.ReadLine();
+                                }
+
                             }
-                        if (line.StartsWith("Date:"))
-                            {
-                            Commits[i-2].Date = line;
-                            }
-                        if (line.StartsWith("commit") != true && line.StartsWith("Author:") != true && line.StartsWith("Date:") != true)
-                            {
-                            Commits[i-3].Massage = line;
-                            }
+
                         }
                     }
+                }
+
             Commits.Sort(CommitComparer.CompareByAutor);
-            string autor = Commits[1].Autor;
+            string autor = Commits[0].Autor;
             Console.WriteLine(autor);
             foreach (Commit commit in Commits)
                 {
                 if (commit.Autor == autor)
                     {
-                    Console.WriteLine(commit.Date);
                     Console.WriteLine(commit.Massage);
                     }
                 if (commit.Autor != autor) 
                     {
                     autor = commit.Autor;
                     Console.WriteLine(autor);
+                    Console.WriteLine(commit.Massage);
                     }
                 }
 
